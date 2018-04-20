552823a9e079fceacf6c5a9f116802aad3939db0
masongzhi <angel001ma@gmail.com>
2018-04-21 03:35:08 +0800

    增加查询语言选项、完善时间选项、增加加载中状态、增加滚动加载

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,25 @@
 //index.js
 //获取应用实例
 const app = getApp()
-// const {repository_search_url} = require('../../api/api.js')
+const {repository_search_url} = require('../../api/api.js')
 const { repository_search_url: data_repository_search_url } = require('./mock.js')
+const { transferRange, transferTimeToStr } = require('../../utils/util.js')
 
 Page({
   data: {
     page: 1,
     time: 'Today',
+    timeStr: transferRange('Today'),
     language: 'JavaScript',
+    languageStr: 'JavaScript',
     items: [],
-    timeArr: ["Today", "Last week", "Last month", "Last year"],
+    timeArr: ["Today", "Last week", "Last month", "Last year"],    
     languageArr: [
       "All",
+      "C#",
+      "C++",
+      "CSS",
+      "CoffeeScript",
       "Go",
       "HTML",
       "Java",
@@ -24,45 +31,65 @@ Page({
   },
   bindTimeChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    
     this.setData({
       page: 1,
-      time: e.detail.value
+      time: this.data.timeArr[e.detail.value],
+      timeStr: transferRange(this.data.timeArr[e.detail.value])
     })
     // 调用getRepository
+    this.getRepository()
   },
   bindLanguageChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       page: 1,
-      language: e.detail.value
+      languageStr: e.detail.value ? this.data.languageArr[e.detail.value] : '',
+      language: this.data.languageArr[e.detail.value]
     })
     // 调用getRepository
+    this.getRepository()
   },
   getRepository: function () {
-    console.log(this.data)
-    // wx.request({
-    //   url: repository_search_url, //仅为示例，并非真实的接口地址
-    //   data: {
-    //     q: `created:>2018-04-18%20language:${this.data.language}`,
-    //     sort: 'stars',
-    //     order: 'desc',
-    //     per_page: 10,
-    //     page: this.data.page
-    //   },
-    //   header: {
-    //     'content-type': 'application/json' // 默认值
-    //   },
-    //   success: function (res) {
-    //     console.log(res.data)
-    //     const data = res.data
-    //     this.setData({
-    //       // 使用mock数据
-    //       // items: data.items
-    //       items: data_repository_search_url
-    //       page: ++page
-    //     })
-    //   }
-    // })
+    wx.showLoading({
+      title: '加载中',
+    })
+    const that = this
+    wx.request({
+      url: repository_search_url,
+      data: {
+        q: `created:>${that.data.timeStr} language:${that.data.language}`,
+        sort: 'stars',
+        order: 'desc',
+        per_page: 10,
+        page: that.data.page
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        const data = res.data
+        let items = []
+        // 区分是否改变条件
+        if (that.data.page !== 1) {
+          items = Array.prototype.concat.call(that.data.items, data.items)
+        } else {
+          items = data.items
+        }
+        that.setData({
+          items: that.getItemsByAddCreateTime(items),
+          page: ++that.data.page
+        })
+        wx.hideLoading()
+      }
+    })
+  },
+  getItemsByAddCreateTime: function (items) {
+    return items.map(it => {
+      it.createTime = transferTimeToStr(it.created_at)
+      return it
+    })
   },
   onLoad: function () {
     wx.getSystemInfo({
@@ -72,11 +99,7 @@ Page({
         })
       }
     })
-    this.setData({
-      // 使用mock数据
-      items: data_repository_search_url.items
-    })
-    console.log(this.data.items)
     // 调用getRepository
+    this.getRepository()
   }
 })
